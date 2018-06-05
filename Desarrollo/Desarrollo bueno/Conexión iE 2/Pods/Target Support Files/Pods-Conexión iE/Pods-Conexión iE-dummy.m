475a1778877930bc877aea41a69a74787736999d
Ivo Sam <ivosam2@hotmail.com>
2018-06-04 19:34:16 -0500

    pods
    pods

---
+++ Desarrollo/Desarrollo bueno/Conexión iE 2/Pods/Target Support Files/Pods-Conexión iE/Pods-Conexión iE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Conexio_n_iE : NSObject
+@end
+@implementation PodsDummy_Pods_Conexio_n_iE
+@end
---
+++ Desarrollo/Desarrollo bueno/Conexión iE 2/Pods/Target Support Files/Pods-Conexión iE/Pods-Conexión iE-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Conexio_n_iEVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Conexio_n_iEVersionString[];
+
